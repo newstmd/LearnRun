63827b68fc1bec9dc51fb418525abd724330b894
newstmd <tangxin@qq.com>
2014-12-04 15:10:38 +0800

    3-1

---
+++ src/app.js
@@ -0,0 +1,40 @@
+var MenuLayer = cc.Layer.extend({
+	ctor:function(){
+		this._super();
+	},
+	init:function(){
+		this._super();
+		
+		var winsize = cc.director.getWinSize();
+		var centerpos = cc.p(winsize.width/2,winsize.height/2);
+		
+		var spritebg = cc.Sprite.create(res.helloBG_png);
+		spritebg.setPosition(centerpos);
+		this.addChild(spritebg);
+		
+		cc.MenuItemFont.setFontSize(60);
+		
+		var menuItemPlay = cc.MenuItemSprite.create(
+				cc.Sprite.create(res.start_n_png),
+				cc.Sprite.create(res.start_s_png),
+				this.onPlay,this
+				);
+		var menu = cc.Menu.create(menuItemPlay);
+		menu.setPosition(centerpos);
+		this.addChild(menu);
+	},
+	onPlay:function(){
+		cc.log("==onplay clicked");
+	}
+});
+
+
+//scene
+var MenuScene = cc.Scene.extend({
+	onEnter:function(){
+		this._super();
+		var layer = new MenuLayer();
+		layer.init();
+		this.addChild(layer);
+	}
+});
