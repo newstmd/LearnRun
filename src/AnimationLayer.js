57aebb24510ba0f4f114e85aa759dd0593168f96
newstmd <tangxin@qq.com>
2014-12-04 16:50:36 +0800

    5-2

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -9,13 +9,28 @@ var AnimationLayer = cc.Layer.extend({
 	init:function(){
 		this._super();
 		
-		var spriteRunner = cc.Sprite.create(res.runner_png);
-		spriteRunner.attr({
-			x:80,
-			y:85
-		});
-		var actionTo = cc.MoveTo.create(2,cc.p(300, 85));
-		spriteRunner.runAction(cc.Sequence.create(actionTo));
-		this.addChild(spriteRunner);
+		// create sprite sheet
+		//cc.spriteFrameCache.addSpriteFrame(frame, frameName)
+		
+		cc.spriteFrameCache.addSpriteFrames(res.runner_plist);
+		this.spriteSheet = cc.SpriteBatchNode.create(res.runner_png);
+		this.addChild(this.spriteSheet);
+		
+		// init runningAction
+		var animFrames = [];
+		for (var i = 0; i < 8; i++) {
+			var str = "runner"+i+".png";
+			var frame = cc.spriteFrameCache.getSpriteFrame(str);
+			animFrames.push(frame);
+		}
+		
+		var animation = cc.Animation.create(animFrames,0.1);
+		this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
+		this.sprite = cc.Sprite.create("#runner0.png");
+		this.sprite.x=80;
+		this.sprite.y=85;
+		this.sprite.runAction(this.runningAction);
+		this.spriteSheet.addChild(this.sprite);
+		
 	}
 });
