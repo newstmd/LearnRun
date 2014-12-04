3d4073a597eefe5393515761faa9d59230d20a94
newstmd <tangxin@qq.com>
2014-12-04 12:18:28 +0800

    1

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,7 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    helloBG_png:"res/helloBG.png",
+    start_n_png:"res/start_n.png",
+    start_s_png:"res/start_s.png"
 };
 
 var g_resources = [];
