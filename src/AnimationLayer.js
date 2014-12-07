0b1280b7b6d72dafe9782f1231d5f1f520084df7
newstmd <tangxin@qq.com>
2014-12-07 17:35:54 +0800

    我改变主意了，JS不好用！

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -2,8 +2,11 @@ var AnimationLayer = cc.Layer.extend({
 	spriteSheet:null,
 	runningAction:null,
 	sprite:null,
-	ctor:function(){
+	space:null,
+	body:null,
+	ctor:function(space){
 		this._super();
+		this.space = space;
 		this.init();
 	},
 	init:function(){
@@ -26,7 +29,19 @@ var AnimationLayer = cc.Layer.extend({
 		
 		var animation = cc.Animation.create(animFrames,0.1);
 		this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
-		this.sprite = cc.Sprite.create("#runner0.png");
+		this.sprite = cc.PhysicsSprite.create("#runner0.png");
+		var contentSize = this.sprite.getContentSize();
+		this.body = new cp.Body(1, cp.momentForBox(1, contentSize.width, contentSize.height));
+		this.body.p = cc.p(g_runnerStartX, g_groundHight+contentSize.height /2);
+		this.body.applyImpulse(cp.v(150, 0),cp.v(0, 0));
+		
+		//this.body.addBody(this.body);
+		this.shape = new cp.BoxShape(this.body, contentSize.width - 14, contentSize.height);
+		//7. add shape to space
+		this.space.addShape(this.shape);
+		//8. set body to the physic sprite
+		this.sprite.setBody(this.body);
+		
 		this.sprite.x=80;
 		this.sprite.y=85;
 		this.sprite.runAction(this.runningAction);
--- src/PlayScene.js
+++ src/PlayScene.js
@@ -1,9 +1,23 @@
 var PlayScene = cc.Scene.extend({
+	space:null,
 	onEnter:function(){
 		this._super();
+		this.initPhysics();
 		
 		this.addChild(new BackgroundLayer());
-		this.addChild(new AnimationLayer());
+		this.addChild(new AnimationLayer(this.space));
 		this.addChild(new StatusLayer());
+		
+		this.scheduleUpdate();
+	},
+	initPhysics:function(){
+		this.space = new cp.Space();
+		this.space.gravity = cp.v(0, -350);
+		
+		var wallBottom = new cp.SegmentShape(this.space.staticBody, 
+				cp.v(0, g_groundHight), 
+				cp.v(4294967295, g_groundHight), 
+				0);
+		this.space.addStaticShape(wallBottom);
 	}
 });
